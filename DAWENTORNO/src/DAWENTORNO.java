6c48e4270aefd6f7ece04aba6ffc1d74d03dcdc6
Nandodaw <Nandodaw@gmail.com>
2020-01-20 13:09:43 +0100

    prueba de creacion de clase

--- DAWENTORNO/src/DAWENTORNO.java
+++ DAWENTORNO/src/DAWENTORNO.java
@@ -10,7 +10,7 @@ public class DAWENTORNO {
 		System.out.println("Fernando lo recibio exitosamente");
 		System.out.println("felix estuvo aqui");
 		System.out.println("El proyecto volvió a Luismi");
-		System.out.println("Fernando prueba del 20/01/2020");
+		System.out.println("Fernando prueba del 20/01/2020.");
 		System.out.println("Finalizar tarea 3");
 	}
 
